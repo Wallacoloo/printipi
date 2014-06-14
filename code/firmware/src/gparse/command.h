b6d8557285bdd4643e14125f30bf825007af8039
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-06-14 07:08:06 +0000

    Fixed const errors

--- code/firmware/src/gparse/command.h
+++ code/firmware/src/gparse/command.h
@@ -11,17 +11,18 @@ class Command {
 	std::vector<std::string> pieces; //the command when split on spaces. Eg "G1 X2 Y3" -> ["G1", "X2", "Y3"]
 	public:
 		//initialize the command object from a line of GCode
-		Command(std::string&);
+		Command() {}
+		Command(std::string const&);
 		/*execute the GCode on a target object that supports a well-defined interface.
 		 *returns a Command to send back to the host.
 		 */
-		template <typename T> Command execute(T& target) {
+		template <typename T> Command execute(T &target) {
 			std::string opcode = getOpcode();
 			Command resp;
 			if (opcode == "M105") {
 				int t, b;
 				target.getTemperature(t, b);
-				resp = Command("ok T:" + to_string(t) + " B:" + to_string(b) + "\n");
+				resp = Command("ok T:" + std::to_string(t) + " B:" + std::to_string(b) + "\n");
 			} else {
 				throw new std::string("unrecognized gcode opcode");
 			}
