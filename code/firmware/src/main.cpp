1c936b5a2c56195ac8c70f9d0d927170e6e9e688
Colin Wallace <wallacoloo@gmail.com>
2014-06-14 06:10:54 +0000

    Revised makefile and gparse structure

--- code/firmware/src/main.cpp
+++ code/firmware/src/main.cpp
@@ -1,41 +1,15 @@
-#include <iostream>
+#include <iostream> //for std::cerr?
 #include <string>
 #include <fcntl.h> //needed for (file) open()
 #include <stdlib.h> //needed for exit()
-#include <stdio.h>
-#include <unistd.h> //for (file) read()
+#include <stdio.h> //for printf?
 
-#include "command.h"
+#include "gparse/serial.h"
+//#include "command.h"
 
 void printUsageAndQuit(char* cmd) {
     std::cerr << "usage: " << cmd << " ttyFile" << std::endl;
     exit(1);
-}
-
-std::string readLine(int fd) {
-	std::string r;
-	char chr;
-	while(read(fd, &chr, 1) == 1 && chr != '\n') {
-		if (chr != '\r') {
-			r += chr;
-			//printf("c: %c\n", chr);
-		}
-	}
-	return r;
-}
-
-void readLoop(int fd) {
-	std::string cmd;
-	//std::string resp = "TEST\n";
-	while (1) {
-		cmd = readLine(fd);
-		printf("command: %s\n", cmd.c_str());
-		Command parsed = Command(cmd);
-		printf("size of command: %i\n", parsed.pieces.size());
-		Command response = parsed.execute("1");
-		std::string resp = response.toGCode();
-		write(fd, resp.c_str(), resp.length());
-	}
 }
 
 int main(int argc, char** argv) {
