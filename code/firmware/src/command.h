9aa155dffb6fb0204d8a00ae395b9ccd6f1e5acd
Colin Wallace <wallacoloo@gmail.com>
2014-06-14 05:54:14 +0000

    Added Command->GCode generation

--- code/firmware/src/command.h
+++ code/firmware/src/command.h
@@ -5,9 +5,18 @@
 #include <vector>
 class Command {
 	public:
-	std::vector<std::string> pieces;
+	std::vector<std::string> pieces; //the command when split on spaces. Eg "G1 X2 Y3" -> ["G1", "X2", "Y3"]
 	public:
+		//initialize the command object from a line of GCode
 		Command(std::string&);
+		/*execute the GCode on a target object that supports a well-defined interface.
+		 *returns a Command to send back to the host.
+		 */
+		template <typename T> Command execute(T& target) {
+			std::string r="TEST2";
+			return Command(r);
+		}
+		std::string toGCode();
 };
 
 
