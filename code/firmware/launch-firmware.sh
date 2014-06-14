3a03bc5de455c2bffd2c53824c5eeac2b3d84237
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-06-14 01:55:50 +0000

    attempt to delete symlink

--- code/firmware/launch-firmware.sh
+++ code/firmware/launch-firmware.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo rm -f /dev/ttyUSB3dp
 socat -d -d pty,raw,echo=0,link=./tty3dpm pty,raw,echo=0,link=./tty3dps &
 sudo ln -s $(pwd)/tty3dpm /dev/ttyUSB3dp
 ./kossel-firmware ./tty3dps
