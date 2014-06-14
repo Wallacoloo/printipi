4f83ebd508947b026b71ec6183a9c5b20bc6e431
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-06-14 07:51:02 +0000

    made gcode opcodes static consts instead of inline literals

--- code/firmware/src/gparse/state.h
+++ code/firmware/src/gparse/state.h
@@ -11,6 +11,11 @@ enum PositionMode {
 class State {
 	PositionMode positionMode = POS_ABSOLUTE;
 	public:
+		static const std::string OP_G90  = "G90";
+		static const std::string OP_G91  = "G91";
+		static const std::string OP_M21  = "M21";
+		static const std::string OP_M105 = "M105";
+		static const std::string OP_M110 = "M110";
 		State() {}
 		void setPositionMode(PositionMode mode);
 		/*execute the GCode on a target object that supports a well-defined interface.
@@ -19,17 +24,17 @@ class State {
 		template <typename T> Command execute(Command const& cmd, T &target) {
 			std::string opcode = cmd.getOpcode();
 			Command resp;
-			if (opcode == "G90") {
+			if (opcode == OP_G90) {
 				setPositionMode(POS_ABSOLUTE);
-			} else if (opcode == "G91") {
+			} else if (opcode == OP_G91) {
 				setPositionMode(POS_RELATIVE);
-			} else if (opcode == "M21") { //initialize SD card.
+			} else if (opcode == OP_M21) { //initialize SD card.
 				resp = Command::OK;
-			} else if (opcode == "M105") { //get temperature
+			} else if (opcode == OP_M105) { //get temperature
 				int t, b;
 				target.getTemperature(t, b);
 				resp = Command("ok T:" + std::to_string(t) + " B:" + std::to_string(b));
-			} else if (opcode == "M110") { //set current line number
+			} else if (opcode == OP_M110) { //set current line number
 				resp = Command::OK;
 			} else {
 				throw new std::string("unrecognized gcode opcode");
