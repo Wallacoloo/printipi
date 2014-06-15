472854ca16b77cef2b55fa2a5944c91226237d81
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-06-15 22:30:08 +0000

    Added (partial) handling of G1, also (untested) parsing of parameters

--- code/firmware/src/gparse/state.cpp
+++ code/firmware/src/gparse/state.cpp
@@ -2,6 +2,9 @@
 
 namespace gparse {
 
+const std::string State::OP_G1   = "G1";
+const std::string State::OP_G20  = "G20";
+const std::string State::OP_G21  = "G21";
 const std::string State::OP_G90  = "G90";
 const std::string State::OP_G91  = "G91";
 const std::string State::OP_M21  = "M21";
@@ -10,6 +13,10 @@ const std::string State::OP_M110 = "M110";
 
 void State::setPositionMode(PositionMode mode) {
 	this->positionMode = mode; 
+}
+
+void State::setUnitMode(LengthUnit mode) {
+	this->unitMode = mode;
 }
 
 }
