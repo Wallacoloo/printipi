b8b368fbc2849ebee7c95372fb4f2ec5655580ce
Colin Wallace <wallacoloo@gmail.com>
2014-07-03 06:06:19 +0000

    driver numAxis() is now a constexpr

--- code/firmware/src/drivers/kossel.h
+++ code/firmware/src/drivers/kossel.h
@@ -8,7 +8,10 @@ namespace drv {
 class Kossel : public Driver {
     public:
         void getTemperature(int &extruder, int& platform) const;
-        std::size_t numAxis() const;
+        //constexpr std::size_t numAxis() const;
+        constexpr std::size_t numAxis() const {
+            return 4; //A, B, C + Extruder
+        }
         float defaultMoveRate() const;
         float defaultFeedRate() const;
         float relativeTimeOfNextStep(int axisIdx, gparse::StepDirection &dir, float x, float y, float z, float e, float velx, float vely, float velz, float velExt) const;
