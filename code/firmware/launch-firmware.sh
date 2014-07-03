7c986e671753de63aa84868212c705fe3992d2a2
Colin Wallace <wallacoloo@gmail.com>
2014-07-03 07:32:43 +0000

    Fix launching of firmware

--- code/firmware/launch-firmware.sh
+++ code/firmware/launch-firmware.sh
@@ -2,4 +2,4 @@
 sudo rm -f /dev/ttyUSB3dp
 socat -d -d pty,raw,echo=0,link=./tty3dpm pty,raw,echo=0,link=./tty3dps &
 sudo ln -s $(pwd)/tty3dpm /dev/ttyUSB3dp
-./kossel-firmware ./tty3dps
+./build/kossel-firmware ./tty3dps
