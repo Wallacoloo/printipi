b75499d7d6e3420f00ccfe348d7d04b4aab66919
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-03 07:52:22 +0000

    Correct chmod settings for socat

--- code/firmware/launch-firmware.sh
+++ code/firmware/launch-firmware.sh
@@ -2,4 +2,6 @@
 sudo rm -f /dev/ttyUSB3dp
 socat -d -d pty,raw,echo=0,link=./tty3dpm pty,raw,echo=0,link=./tty3dps &
 sudo ln -s $(pwd)/tty3dpm /dev/ttyUSB3dp
+#Octoprint needs to be able to read from its tty:
+sudo chmod +r ./tty3dpm ./tty3dps
 ./build/kossel-firmware ./tty3dps
