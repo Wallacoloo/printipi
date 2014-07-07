77f31f122b395e9c0da68351198bd19be0d883cb
Colin Wallace <wallacoloo@gmail.com>
2014-07-07 06:22:11 +0000

    added AxisStepper::getNextTime

--- code/firmware/src/drivers/axisstepper.h
+++ code/firmware/src/drivers/axisstepper.h
@@ -2,6 +2,7 @@
 #define DRIVERS_AXISSTEPPER_H
 
 #include "../event.h"
+#include <tuple>
 
 namespace drv {
 
@@ -9,7 +10,34 @@ class AxisStepper {
 	public:
 		float time;
 		StepDirection direction;
+		template <typename TupleT> static AxisStepper& getNextTime(TupleT &axes);
+		
 };
+
+//Helper classes for AxisStepper::getNextTime method
+//C++ doesn't support partial template function specialization, so we need to use templated classes instead.
+//Below function(s) select the AxisStepper with the smallest time attribute from a tuple of such AxisSteppers
+template <typename TupleT, int idx> class _AxisStepper__getNextTime {
+	public:
+		AxisStepper& operator()(TupleT &axes) {
+			AxisStepper &m1 = _AxisStepper__getNextTime<TupleT, idx-1>()(axes);
+			AxisStepper &m2 = std::get<idx>(axes);
+			return (m1.time < m2.time) ? m1 : m2;
+		}
+};
+
+template <typename TupleT> class _AxisStepper__getNextTime<TupleT, 0> {
+	public:
+		AxisStepper& operator()(TupleT &axes) {
+			return std::get<0>(axes);
+			
+		}
+};
+
+template <typename TupleT> AxisStepper& AxisStepper::getNextTime(TupleT &axes) {
+	return _AxisStepper__getNextTime<TupleT, std::tuple_size<TupleT>::value-1>()(axes);
+}
+
 
 }
 
