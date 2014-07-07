89cc3c26cb804c5bb79ef818bb426297f74a8b5e
Colin Wallace <wallacoloo@gmail.com>
2014-07-07 21:39:48 +0000

    Enums are now in typesettings.h

--- code/firmware/src/typesettings.h
+++ code/firmware/src/typesettings.h
@@ -1,1 +1,22 @@
+#ifndef TYPESETTINGS_H
+#define TYPESETTINGS_H
+
 typedef char AxisIdType;
+
+enum PositionMode {
+	POS_ABSOLUTE,
+	POS_RELATIVE,
+	POS_UNDEFINED //Sadly, need a way to tie extruder coords to positioning coords in the case that it's undefined.
+};
+
+enum LengthUnit {
+	UNIT_MM,
+	UNIT_IN
+};
+
+enum StepDirection {
+	StepForward,
+	StepBackward
+};
+
+#endif
