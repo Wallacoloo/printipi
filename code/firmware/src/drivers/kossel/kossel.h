8374397f7371410c93494ee78fcfbf52cce30721
Colin Wallace <wallacoloo@gmail.com>
2014-07-07 23:27:38 +0000

    Use enum for CoordType. Also more logging

--- code/firmware/src/drivers/kossel/kossel.h
+++ code/firmware/src/drivers/kossel/kossel.h
@@ -13,7 +13,7 @@ class Kossel : public Driver {
     public:
         void getTemperature(int &extruder, int& platform) const;
         //typedef std::tuple<AxisStepper, AxisStepper, AxisStepper, ExtruderStepper<20000> > AxisSteppers;
-        typedef std::tuple<LinearStepper<20000, 'x'>, LinearStepper<20000, 'y'>, LinearStepper<20000, 'z'>, LinearStepper<20000, 'e'> > AxisSteppers;
+        typedef std::tuple<LinearStepper<20000, COORD_X>, LinearStepper<20000, COORD_Y>, LinearStepper<20000, COORD_Z>, LinearStepper<20000, COORD_E> > AxisSteppers;
         constexpr static std::size_t numAxis() {
             return 4; //A, B, C + Extruder
         }
