f7b64713461eea4a93f4d5b71dc08372232bf675
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-08 00:32:24 +0000

    Fix issue with abs instead of fabs?

--- code/firmware/src/gmath.h
+++ code/firmware/src/gmath.h
@@ -1,9 +1,32 @@
 #ifndef GMATH_H
 #define GMATH_H
 
+#include <limits>
+
 namespace gmath {
 
 const float MM_PER_IN = 25.4; //It appears that by definition, there are EXACTLY 25.4 mm per inch.
+const float NANOSECOND = 1e-9;
+const float EPSILON = std::numeric_limits<float>::epsilon(); //represents off-by-one LSB for float value=1.0
+
+template <typename T1, typename T2> constexpr bool ltepsilon(T1 a, T2 b, float epsilon=EPSILON) {
+	//return a < b + epsilon;
+	return a - b < epsilon;
+}
+
+template <typename T1, typename T2> constexpr bool eqepsilon(T1 a, T2 b, float epsilon=EPSILON) {
+	//return a < b + epsilon;
+	return std::fabs(a - b) <= epsilon;
+}
+
+template <typename T1> T1 makeZeroIfClose(T1 a, float epsilon=NANOSECOND) {
+	if (eqepsilon(a, 0, epsilon)) {
+		return 0;
+	} else {
+		return a;
+	}
+}
+
 
 }
 
