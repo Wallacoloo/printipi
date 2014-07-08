b44b2344760fdb6f69738d76cf20c446188cd8c4
Colin Wallace <wallacoloo@gmail.com>
2014-07-08 04:00:17 +0000

    Implemented Event constructor, added outline for Scheduler consumer thread

--- code/firmware/src/event.cpp
+++ code/firmware/src/event.cpp
@@ -11,7 +11,12 @@ const struct timespec& Event::time() const {
 	return this->_time;
 }
 
-Event Event::StepperEvent(float relTime, char stepperNum, StepDirection dir) {
-	return Event();
+Event::Event(const timespec &t, AxisIdType stepperNum, StepDirection dir) : _time(t), _stepperNum(stepperNum), _isForward(dir==StepForward) {}
+
+Event Event::StepperEvent(float relTime, AxisIdType stepperNum, StepDirection dir) {
+	struct timespec t;
+	t.tv_sec = relTime;
+	t.tv_nsec = (relTime-t.tv_sec)*1000000000;
+	return Event(t, stepperNum, dir);
 }
 
