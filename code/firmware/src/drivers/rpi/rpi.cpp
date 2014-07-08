87f8ec307d9aa03fcd8a66c65986d060ebd0ff3a
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-08 08:02:23 +0000

    bcm2835_close is run at exit

--- code/firmware/src/drivers/rpi/rpi.cpp
+++ code/firmware/src/drivers/rpi/rpi.cpp
@@ -1,16 +1,23 @@
 #include "rpi.h"
 #include "bcm2835.h"
 
+#include <cstdlib> //for atexit
+
 namespace drv {
 namespace rpi {
 
 bool wasBcmInit = false;
+
+void atexit_bcm2835_close() {
+	bcm2835_close(); //return value is int.
+}
 
 void initIO() {
 	if (!wasBcmInit) {
 		wasBcmInit = true;
 		//wiringPiSetup();
 		bcm2835_init();
+		std::atexit(atexit_bcm2835_close);
 		//atexit: bcm2835_close
 	}
 }
