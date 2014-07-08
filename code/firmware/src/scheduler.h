a656ee572bc122cbc45e21fe5dcc2633925b949f
Colin Wallace <wallacoloo@gmail.com>
2014-07-08 10:23:36 +0000

    cap scheduler size

--- code/firmware/src/scheduler.h
+++ code/firmware/src/scheduler.h
@@ -15,15 +15,23 @@
 #ifndef SCHED_PRIORITY
 #define SCHED_PRIORITY 30
 #endif
+#ifndef SCHED_CAPACITY
+#define SCHED_CAPACITY 1024
+#endif
+/*#ifndef SCHED_PUSH_SPIN_INTERVAL
+#define SCHED_PUSH_SPIN_INTERVAL 100 //msec
+#endif*/
 
 
 class Scheduler {
 	std::queue<Event> eventQueue;
 	std::mutex mutex;
+	std::mutex allowPushMutex; //lock this when capacity is exceeded.
+	bool _isPushLocked;
 	std::condition_variable nonemptyCond;
 	public:
 		void queue(const Event& evt);
-		//Scheduler();
+		Scheduler();
 		Event nextEvent();
 };
 
