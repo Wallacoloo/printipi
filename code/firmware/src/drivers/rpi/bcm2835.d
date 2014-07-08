487783453fbea24d74fe188ab8ba6160404d4bc5
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-08 06:32:05 +0000

    removed .d file

--- code/firmware/src/drivers/rpi/bcm2835.d
+++
@@ -1,6 +0,0 @@
-../build/debug/drivers/rpi/bcm2835.o drivers/rpi/bcm2835.d: \
- drivers/rpi/bcm2835.cpp drivers/rpi/bcm2835.c drivers/rpi/bcm2835.h
-
-drivers/rpi/bcm2835.c:
-
-drivers/rpi/bcm2835.h:
