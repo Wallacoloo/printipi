d9a7a21ef09df186300b90a1b194169cd0d22115
Colin Wallace <wallacoloo@gmail.com>
2014-07-08 10:09:36 +0000

    Cleaned up commented code

--- code/firmware/src/scheduler.cpp
+++ code/firmware/src/scheduler.cpp
@@ -8,7 +8,7 @@
 /*Scheduler::Scheduler(const std::function<void(const Event&)>& callback) : consumer(std::thread(&Scheduler::consumerLoop, this, callback)) {}*/
 
 //Scheduler::Scheduler() : consumer(std::thread(&Scheduler::consumerLoop, this)) {}
-Scheduler::Scheduler() {}
+//Scheduler::Scheduler() {}
 
 
 void Scheduler::queue(const Event& evt) {
@@ -18,31 +18,6 @@ void Scheduler::queue(const Event& evt) {
 	this->nonemptyCond.notify_one(); //notify the consumer thread that a new event is ready.
 }
 
-/*void Scheduler::consumerLoop(const std::function<void(const Event&)>& callback) {
-	LOGD("Scheduler::consumerLoop begin\n");
-	
-	struct sched_param sp; //set high priority for the scheduling thread.
-    sp.sched_priority=SCHED_PRIORITY; 
-    int retval = pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp);
-    if (retval) {
-    	LOGW("Warning: pthread_setschedparam (set thread to high priority) in Scheduler::consumerLoop returned non-zero: %i\n", retval);
-    }
-    LOGD("Scheduler::consumerLoop priority set to %i\n", SCHED_PRIORITY);
-    
-	Event evt;
-	while (1) {
-		{
-			std::unique_lock<std::mutex> lock(this->mutex);
-			while (this->eventQueue.empty()) { //wait for an event to be pushed.
-				this->nonemptyCond.wait(lock); //condition_variable.wait() can produce spurious wakeups; need the while loop.
-			}
-			evt = this->eventQueue.front();
-			this->eventQueue.pop();
-		} //unlock the mutex and then handle the event.
-		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &(evt.time()), NULL); //sleep to event time.
-		callback(evt); //process the event.
-	}
-}*/
 
 Event Scheduler::nextEvent() {
 	Event evt;
@@ -58,28 +33,3 @@ Event Scheduler::nextEvent() {
 	return evt;
 }
 
-/*void Scheduler::consumerLoop() {
-	LOGD("Scheduler::consumerLoop begin\n");
-	
-	struct sched_param sp; //set high priority for the scheduling thread.
-    sp.sched_priority=SCHED_PRIORITY; 
-    int retval = pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp);
-    if (retval) {
-    	LOGW("Warning: pthread_setschedparam (set thread to high priority) in Scheduler::consumerLoop returned non-zero: %i\n", retval);
-    }
-    LOGD("Scheduler::consumerLoop priority set to %i\n", SCHED_PRIORITY);
-    
-	Event evt;
-	while (1) {
-		{
-			std::unique_lock<std::mutex> lock(this->mutex);
-			while (this->eventQueue.empty()) { //wait for an event to be pushed.
-				this->nonemptyCond.wait(lock); //condition_variable.wait() can produce spurious wakeups; need the while loop.
-			}
-			evt = this->eventQueue.front();
-			this->eventQueue.pop();
-		} //unlock the mutex and then handle the event.
-		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &(evt.time()), NULL); //sleep to event time.
-		//callback(evt); //process the event.
-	}
-}*/
--- code/firmware/src/scheduler.h
+++ code/firmware/src/scheduler.h
@@ -19,47 +19,12 @@
 
 class Scheduler {
 	std::queue<Event> eventQueue;
-	std::thread consumer;
 	std::mutex mutex;
 	std::condition_variable nonemptyCond;
 	public:
 		void queue(const Event& evt);
-		//Scheduler(const std::function<void(const Event&)>& callback);
-		Scheduler();
+		//Scheduler();
 		Event nextEvent();
-	private:
-		//void consumerLoop();
-		//void consumerLoop(const std::function<void(const Event&)>& callback);
-	public:
-		//template <typename T> Scheduler(T& callback) : consumer(std::thread(&Scheduler::consumerLoop<T>, this, callback)) {}
-		/*template <typename T> Scheduler(T& callback) : consumer(std::thread([&]{ consumerLoop(callback); })) {}
-		template <typename T> void consumerLoop(T& callback) {
-			LOGD("Scheduler::consumerLoop begin\n");
-	
-			struct sched_param sp; //set high priority for the scheduling thread.
-			sp.sched_priority=SCHED_PRIORITY; 
-			int retval = pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp);
-			if (retval) {
-				LOGW("Warning: pthread_setschedparam (set thread to high priority) in Scheduler::consumerLoop returned non-zero: %i\n", retval);
-			}
-			LOGD("Scheduler::consumerLoop priority set to %i\n", SCHED_PRIORITY);
-		
-			Event evt;
-			while (1) {
-				{
-					std::unique_lock<std::mutex> lock(this->mutex);
-					while (this->eventQueue.empty()) { //wait for an event to be pushed.
-						this->nonemptyCond.wait(lock); //condition_variable.wait() can produce spurious wakeups; need the while loop.
-					}
-					evt = this->eventQueue.front();
-					this->eventQueue.pop();
-				} //unlock the mutex and then handle the event.
-				clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &(evt.time()), NULL); //sleep to event time.
-				callback.handleEvent(evt);
-				//callback(evt); //process the event.
-			}
-		}*/
-		
 };
 
 
