a7c6910c070e3535c2afc8e57dd67223f97b5ca1
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-08 04:00:17 +0000

    Implemented Event constructor, added outline for Scheduler consumer thread

--- code/firmware/src/scheduler.cpp
+++ code/firmware/src/scheduler.cpp
@@ -4,8 +4,14 @@
 #include "logging.h"
 
 
+Scheduler::Scheduler() : consumer(std::thread(&Scheduler::consumerLoop, this)) {
+}
+
 void Scheduler::queue(const Event& evt) {
 	LOGV("Scheduler::queue\n");
 	this->eventQueue.push(evt);
 }
 
+void Scheduler::consumerLoop() {
+	LOGD("Scheduler::consumerLoop begin");
+}
--- code/firmware/src/scheduler.h
+++ code/firmware/src/scheduler.h
@@ -2,13 +2,18 @@
 #define SCHEDULER_H
 
 #include <queue>
+#include <thread>
 #include "event.h"
 
 
 class Scheduler {
 	std::queue<Event> eventQueue;
+	std::thread consumer;
 	public:
 		void queue(const Event& evt);
+		Scheduler();
+	private:
+		void consumerLoop();
 
 };
 
