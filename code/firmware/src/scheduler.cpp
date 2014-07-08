70e68ffd90743dd0a3a66cd9be8fe2d167793b31
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-08 10:23:36 +0000

    cap scheduler size

--- code/firmware/src/scheduler.cpp
+++ code/firmware/src/scheduler.cpp
@@ -8,11 +8,20 @@
 /*Scheduler::Scheduler(const std::function<void(const Event&)>& callback) : consumer(std::thread(&Scheduler::consumerLoop, this, callback)) {}*/
 
 //Scheduler::Scheduler() : consumer(std::thread(&Scheduler::consumerLoop, this)) {}
-//Scheduler::Scheduler() {}
+Scheduler::Scheduler() : _isPushLocked(false) {}
 
 
 void Scheduler::queue(const Event& evt) {
 	LOGV("Scheduler::queue\n");
+	/*do {
+		std::unique_lock<std::mutex> lock(this->mutex);
+		if (this->eventQueue.size() < SCHED_CAPACITY) {
+			this->eventQueue.push(evt);
+			break;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(SCHED_PUSH_SPIN_INTERVAL));
+	} while(1);*/
+	std::unique_lock<std::mutex> requestAccess(this->allowPushMutex);
 	std::unique_lock<std::mutex> lock(this->mutex);
 	this->eventQueue.push(evt);
 	this->nonemptyCond.notify_one(); //notify the consumer thread that a new event is ready.
@@ -28,6 +37,13 @@ Event Scheduler::nextEvent() {
 		}
 		evt = this->eventQueue.front();
 		this->eventQueue.pop();
+		if (this->eventQueue.size() >= SCHED_CAPACITY && !this->_isPushLocked) {
+			this->allowPushMutex.lock();
+			this->_isPushLocked = true;
+		} else if (this->eventQueue.size() == SCHED_CAPACITY-1 && this->_isPushLocked){
+			this->allowPushMutex.unlock();
+			this->_isPushLocked = false;
+		}
 	} //unlock the mutex and then handle the event.
 	clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &(evt.time()), NULL); //sleep to event time.
 	return evt;
