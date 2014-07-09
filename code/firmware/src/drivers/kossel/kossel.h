87caa852672bfee0fd18b2acfd2ea0dc10ffb96f
Colin Wallace <wallacoloo@gmail.com>
2014-07-09 06:39:25 +0000

    200000 steps/mm for x direction

--- code/firmware/src/drivers/kossel/kossel.h
+++ code/firmware/src/drivers/kossel/kossel.h
@@ -13,7 +13,7 @@ namespace drv {
 
 class Kossel : public Driver {
     public:
-        typedef std::tuple<LinearStepper<1000, COORD_X>, LinearStepper<1000, COORD_Y>, LinearStepper<1000, COORD_Z>, LinearStepper<1000, COORD_E> > AxisSteppers;
+        typedef std::tuple<LinearStepper<200000, COORD_X>, LinearStepper<1000, COORD_Y>, LinearStepper<1000, COORD_Z>, LinearStepper<1000, COORD_E> > AxisSteppers;
         typedef std::tuple<
         	//rpi::A4988<RPI_GPIO_P1_11, RPI_GPIO_P1_12>,
         	rpi::SN754410<RPI_V2_GPIO_P1_13, RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_16, RPI_V2_GPIO_P1_18>, //X coord
