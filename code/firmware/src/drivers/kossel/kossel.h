32c53d0446bc75a3399ffb71b7536214ef0caa08
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-10 22:18:01 +0000

    LinearDeltaStepper works with mm

--- code/firmware/src/drivers/kossel/kossel.h
+++ code/firmware/src/drivers/kossel/kossel.h
@@ -12,6 +12,9 @@
 #include "drivers/lineardeltacoordmap.h"
 #include <tuple>
 
+//R1000 = distance from (0, 0) (platform center) to each axis, in micrometers (1e-6)
+//L1000 = length of the rods that connect each axis to the end effector
+//STEPS_M = #of steps for the motor driving each axis (A, B, C) to raise its carriage by 1 meter.
 #define R1000 100000
 #define L1000 260000
 #define STEPS_M 200000
@@ -22,7 +25,7 @@ class Kossel : public Driver {
     public:
         //typedef std::tuple<LinearStepper<10000, COORD_X>, LinearStepper<1000, COORD_Y>, LinearStepper<1000, COORD_Z>, LinearStepper<1000, COORD_E> > AxisStepperTypes;
         typedef LinearDeltaCoordMap<0, 1, 2, 3, R1000, L1000, STEPS_M> CoordMapT;
-        typedef std::tuple<LinearDeltaStepper<0, CoordMapT, R1000, L1000>, LinearDeltaStepper<1, CoordMapT, R1000, L1000>, LinearDeltaStepper<2, CoordMapT, R1000, L1000>, LinearStepper<1000, COORD_E> > AxisStepperTypes;
+        typedef std::tuple<LinearDeltaStepper<0, CoordMapT, R1000, L1000, STEPS_M>, LinearDeltaStepper<1, CoordMapT, R1000, L1000, STEPS_M>, LinearDeltaStepper<2, CoordMapT, R1000, L1000, STEPS_M>, LinearStepper<1000, COORD_E> > AxisStepperTypes;
         typedef std::tuple<
         	//rpi::A4988<RPI_GPIO_P1_11, RPI_GPIO_P1_12>,
         	rpi::SN754410<RPI_V2_GPIO_P1_13, RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_16, RPI_V2_GPIO_P1_18>, //X coord
