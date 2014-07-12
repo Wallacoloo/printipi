91c1e27b30e55be821a9be66af375ffd0eafe747
Colin Wallace <wallacoloo@gmail.com>
2014-07-12 08:05:17 +0000

    more logging

--- code/firmware/src/drivers/rpi/onepinenabler.h
+++ code/firmware/src/drivers/rpi/onepinenabler.h
@@ -9,10 +9,12 @@ namespace rpi {
 template <uint8_t Pin, bool ValueEn, bool ValueDis> class OnePinEnabler : public EnableDisableDriver {
 	public:
 		static void enable() {
+			EnableDisableDriver::enable();
 			bcm2835_gpio_fsel(Pin, BCM2835_GPIO_FSEL_OUTP); //configure this pins as output
 			bcm2835_gpio_write(Pin, ValueEn);
 		}
 		static void disable() {
+			EnableDisableDriver::disable();
 			bcm2835_gpio_write(Pin, ValueDis);
 		}
 		/*static void deactivate() { //called on shutdown.
