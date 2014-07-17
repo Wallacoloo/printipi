aed50265b1fe9e89e695f448cbf0c20cac9a038e
Colin Wallace <wallacoloo@gmail.com>
2014-07-17 07:13:29 +0000

    Log on rpi unload

--- code/firmware/src/drivers/rpi/rpi.cpp
+++ code/firmware/src/drivers/rpi/rpi.cpp
@@ -3,6 +3,7 @@
 
 //#include <cstdlib> //for atexit
 #include "scheduler.h"
+#include "logging.h"
 
 namespace drv {
 namespace rpi {
@@ -10,6 +11,7 @@ namespace rpi {
 bool wasBcmInit = false;
 
 void atexit_bcm2835_close() {
+	LOG("freeing bcm2835 memmaps\n");
 	bcm2835_close(); //return value is int. Must be wrapped as void function for std::atexit to be used.
 }
 
