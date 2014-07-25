d416541938be4174b4f6419abffb7f40fef40fa3
Colin Wallace <wallacoloo@gmail.com>
2014-07-25 21:42:25 +0000

    Integrated thermistor reading into new scheduler system (untested)

--- code/firmware/src/drivers/tempcontrol.h
+++ code/firmware/src/drivers/tempcontrol.h
@@ -2,19 +2,38 @@
 #define DRIVERS_TEMPCONTROL_H
 
 #include "drivers/iodriver.h"
+#include "timeutil.h"
 
 namespace drv {
 
 template <typename IOFace, typename Thermistor> class TempControl : public IODriver {
 	Thermistor _therm;
 	float _lastTemp;
+	bool _isReading;
+	struct timespec _nextReadTime;
+	struct timespec _interval;
 	public:
-		TempControl() : IODriver(this), _lastTemp(0) {
+		TempControl() : IODriver(this), _lastTemp(0), _isReading(false), _nextReadTime(timespecNow()), _interval{1, 0} {
 		}
-		bool onIdleCpu() const {
-			LOGV("TempControl::onIdleCpu()\n");
-			return false;
-			//_therm.onIdleCp
+		bool onIdleCpu() {
+			//LOGV("TempControl::onIdleCpu()\n");
+			if (_isReading) {
+				if (_therm.isReady()) {
+					_lastTemp = _therm.value();
+					return false; //no more cpu needed.
+				} else {
+					return true; //need more cpu time.
+				}
+			} else {
+				struct timespec now = timespecNow();
+				if (timespecLt(_nextReadTime, now)) { //time for another read
+					_nextReadTime = timespecAdd(now, _interval);
+					_therm.startRead();
+					return true; //more cpu time needed.
+				} else {
+					return false;
+				}
+			}
 		}
 		float getLastTemp() const {
 			return _lastTemp;
