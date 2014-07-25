f0c22e3355db9d932b2abf98c1d982a6c85a830b
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-25 23:53:19 +0000

    Typo fix

--- code/firmware/src/drivers/kossel/kossel.h
+++ code/firmware/src/drivers/kossel/kossel.h
@@ -61,7 +61,7 @@ class Kossel : public Driver {
 		typedef rpi::LeverEndstop<RPI_V2_GPIO_P1_24, 0, BCM2835_GPIO_PUD_DOWN> _EndstopB;
 		typedef rpi::LeverEndstop<RPI_V2_GPIO_P1_26, 0, BCM2835_GPIO_PUD_DOWN> _EndstopC;
 		typedef rpi::RCThermistor<RPI_V2_GPIO_P1_07, THERM_RA, THERM_CAP_PICO, VCC_mV, THERM_IN_THRESH_mV, THERM_T0, THERM_R0, THERM_BETA> _Thermistor;
-		typedef rpi::OnePinIODriver<RPI_V2_GPIO_P1_8, 1> _Fan;
+		typedef rpi::OnePinIODriver<RPI_V2_GPIO_P1_08, 1> _Fan;
 		typedef rpi::OnePinIODriver<RPI_V2_GPIO_P1_10, 1> _HotendOut;
 		//typedef TempControl<_HotendOut, _Thermistor> _HotendController;
     public:
