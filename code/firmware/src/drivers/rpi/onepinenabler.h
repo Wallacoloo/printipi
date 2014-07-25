6c6275f82e6e0bb74e5f932176019537cd5fe8b5
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-25 19:20:02 +0000

    Missing initIO (Although even now, it may never be called if object isn't instantiated

--- code/firmware/src/drivers/rpi/onepinenabler.h
+++ code/firmware/src/drivers/rpi/onepinenabler.h
@@ -8,6 +8,9 @@ namespace rpi {
 
 template <uint8_t Pin, bool ValueEn, bool ValueDis=!ValueEn> class OnePinEnabler : public EnableDisableDriver {
 	public:
+		OnePinEnabler() : EnableDisableDriver(this) {
+			initIO();
+		}
 		static void enable() {
 			EnableDisableDriver::enable();
 			bcm2835_gpio_fsel(Pin, BCM2835_GPIO_FSEL_OUTP); //configure this pins as output
