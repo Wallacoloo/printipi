1e7ae0077cdd9c50dde82c48f72e3f9491d4f5ab
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-26 04:38:27 +0000

    Extruder dimensioning fix

--- code/firmware/src/drivers/kossel/kossel.h
+++ code/firmware/src/drivers/kossel/kossel.h
@@ -27,7 +27,7 @@
 #define H1000 500000
 //#define STEPS_M 9000
 #define STEPS_M 4500 //no microstepping enabled.
-#define STEPS_M_EXT 1800
+#define STEPS_M_EXT 18000
 
 #define THERM_RA 665
 #define THERM_CAP_PICO 100000
