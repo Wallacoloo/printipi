b0e73f0cf41eecb0ceb0411cc5f2b4200bf61690
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-26 03:39:50 +0000

    adjusted steps/m for extruder

--- code/firmware/src/drivers/kossel/kossel.h
+++ code/firmware/src/drivers/kossel/kossel.h
@@ -27,7 +27,7 @@
 #define H1000 500000
 //#define STEPS_M 9000
 #define STEPS_M 4500 //no microstepping enabled.
-#define STEPS_M_EXT 1000
+#define STEPS_M_EXT 1800
 
 #define THERM_RA 665
 #define THERM_CAP_PICO 100000
