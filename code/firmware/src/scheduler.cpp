1520028f2d7ea762d6e403b53e04f150aaeca2c4
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-27 00:48:27 +0000

    Tidied up

--- code/firmware/src/scheduler.cpp
+++ code/firmware/src/scheduler.cpp
@@ -62,7 +62,7 @@ void Scheduler::registerExitHandler(void (*handler)(), unsigned level) {
 }
 
 
-Scheduler::Scheduler() : _lockPushes(mutex, std::defer_lock), _arePushesLocked(false), bufferSize(SCHED_CAPACITY) {
+Scheduler::Scheduler() : bufferSize(SCHED_CAPACITY) {
 	//clock_gettime(CLOCK_MONOTONIC, &(this->lastEventHandledTime)); //initialize to current time.
 }
 
@@ -144,60 +144,6 @@ Event Scheduler::nextEvent(bool doSleep, std::chrono::microseconds timeout) {
 	}
 	
 	return evt;
-	/*Event evt;
-	if (!this->_arePushesLocked) { //Lock other threads from pushing to queue, if not done already.
-		_lockPushes.lock();
-	}
-	while (this->eventQueue.empty()) { //wait for an event to be pushed.
-		//condition_variable.wait() can produce spurious wakeups; need the while loop.
-		//this->nonemptyCond.wait(_lockPushes); //condition_variable.wait() can produce spurious wakeups; need the while loop.
-		if (this->nonemptyCond.wait_for(_lockPushes, timeout) == std::cv_status::timeout) { 
-			if (!this->_arePushesLocked) { //if pushes aren't locked, then unlock the physical lock.
-				_lockPushes.unlock();
-			}
-			return Event(); //return null event
-		}
-	}
-	evt = this->eventQueue.front();
-	this->eventQueue.pop_front();
-	//check if event is PWM-based:
-	//if (pwmInfo[evt.stepperId()].nsLow != 0 || pwmInfo[evt.stepperId()].nsHigh != 0) {
-	if (pwmInfo[evt.stepperId()].isNonNull()) {
-		if (evt.direction() == StepForward) {
-			//if (pwmInfo[evt.stepperId()].nsLow != 0) { //do we need to transition to a low time again? If not, then stay here forever.
-				//next event will be StepBackward, or refresh this event if there is no off-duty.
-				Event nextPwm(evt.time(), evt.stepperId(), pwmInfo[evt.stepperId()].nsLow ? StepBackward : StepForward);
-				nextPwm.offsetNano(pwmInfo[evt.stepperId()].nsHigh);
-				this->orderedInsert(nextPwm); //to do: ordered insert
-			//}
-		} else {
-			//if (pwmInfo[evt.stepperId()].nsHigh != 0) { //do we need to transition to a high time again? If not, then stay here forever.
-				//next event will be StepForward, or refresh this event if there is no on-duty.
-				Event nextPwm(evt.time(), evt.stepperId(), pwmInfo[evt.stepperId()].nsHigh ? StepForward : StepBackward);
-				nextPwm.offsetNano(pwmInfo[evt.stepperId()].nsLow);
-				this->orderedInsert(nextPwm);
-			//}
-		}
-	}
-	if (this->eventQueue.size() < bufferSize) { //queue is underfilled; release the lock
-		_lockPushes.unlock();
-		this->_arePushesLocked = false;
-		eventConsumedCond.notify_one();
-	} else { //queue is filled; do not release the lock.
-		this->_arePushesLocked = true;
-	}
-	if (doSleep) {
-		this->sleepUntilEvent(evt);
-		/*struct timespec sleepUntil = evt.time();
-		//struct timespec curTime;
-		//clock_gettime(CLOCK_MONOTONIC, &curTime);
-		//LOGV("Scheduler::nextEvent sleep from %lu.%lu until %lu.%lu\n", curTime.tv_sec, curTime.tv_nsec, sleepUntil.tv_sec, sleepUntil.tv_nsec);
-		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &sleepUntil, NULL); //sleep to event time.
-		//clock_gettime(CLOCK_MONOTONIC, &(this->lastEventHandledTime)); //in case we fall behind, preserve the relative time between events.
-		//this->lastEventHandledTime = sleepUntil;*/
-	//}
-	
-	//return evt;
 }
 
 void Scheduler::sleepUntilEvent(const Event &evt) const {
