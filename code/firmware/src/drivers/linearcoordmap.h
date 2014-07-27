30d7ded4a783d911d67c68dc808094b1d9bb3f5f
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-27 01:23:33 +0000

    Fixed other coordmap implementations

--- code/firmware/src/drivers/linearcoordmap.h
+++ code/firmware/src/drivers/linearcoordmap.h
@@ -7,11 +7,8 @@ namespace drv {
 
 template <std::size_t xIdx, std::size_t yIdx, std::size_t zIdx, std::size_t eIdx> class LinearCoordMap : public CoordMap {
 	public:
-		template <std::size_t size> static void xyzeFromMechanical(const std::array<int, size> &mech, float &x, float &y, float &z, float &e) {
-			x = mech[xIdx];
-			y = mech[yIdx];
-			z = mech[zIdx];
-			e = mech[eIdx];
+		template <std::size_t size> static void xyzeFromMechanical(const std::array<int, size> &mech) {
+			return std::make_tuple(mech[xIdx], mech[yIdx], mech[zIdx], mech[eIdx]);
 		}
 
 };
