c6e70b123212eebad137bf60fea34712a64bcbd3
Colin Wallace <wallacoloo@gmail.com>
2014-07-29 01:57:50 +0000

    numAxis is now a part of the coord map

--- code/firmware/src/drivers/coordmap.h
+++ code/firmware/src/drivers/coordmap.h
@@ -8,6 +8,7 @@ namespace drv {
 class CoordMap {
 	public:
 		//static std::tuple<float, float, float, float> xyzeFromMechanical(const std::array<int, N>&);
+		//constexpr static std::size_t numAxis();
 };
 
 }
