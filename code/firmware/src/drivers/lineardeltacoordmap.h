63b8b3235a666352f581b2ad6408c6d917292a1b
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-30 04:31:03 +0000

    Fix pedantic errors

--- code/firmware/src/drivers/lineardeltacoordmap.h
+++ code/firmware/src/drivers/lineardeltacoordmap.h
@@ -40,7 +40,7 @@ template <unsigned R1000, unsigned L1000, unsigned H1000, unsigned STEPS_M, unsi
 		static constexpr std::array<int, 4> getHomePosition() {
 			//z = A*MM_STEPS-sqrt(L*L-r*r);
 			//A=(z+sqrt(L*L-r*r))*STEPS_MM
-			return std::array<int, 4>({(int)(h*STEPS_MM), (int)(h*STEPS_MM), (int)(h*STEPS_MM), 0});
+			return std::array<int, 4>({{(int)(h*STEPS_MM), (int)(h*STEPS_MM), (int)(h*STEPS_MM), 0}});
 		}
 		static std::tuple<float, float, float, float> xyzeFromMechanical(const std::array<int, 4> &mech) {
 			float e = mech[EIdx]*MM_STEPS_EXT;
