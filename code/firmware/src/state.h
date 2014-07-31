4e0a189f85cd744931137845a57baf1985e0bab4
wallacoloo <wallacoloo@96c8ad89-c336-4768-9281-ad5d3dc6486f>
2014-07-31 03:38:01 +0000

    Attempt to fix the infinite time error

--- code/firmware/src/state.h
+++ code/firmware/src/state.h
@@ -426,13 +426,22 @@ template <typename Drv> gparse::Command State<Drv>::execute(gparse::Command cons
 template <typename Drv> float State<Drv>::transformEventTime(float time, float moveDuration, float maxVel) {
 	float Amax = this->driver.maxAccel();
 	float Vmax = maxVel; //;30;
-	float V0 = 0.1;
+	float V0 = std::max(0.5*maxVel, 0.1); //c becomes invalid if V0 >= Vmax
 	float k = 4*Amax/Vmax;
 	float c = V0 / (Vmax-V0);
 	if (time > 0.5*moveDuration) {
 		return 2*transformEventTime(0.5*moveDuration, moveDuration, maxVel) - transformEventTime(moveDuration-time, moveDuration, maxVel);
 	} else { //take advantage of the fact that comparisons against NaN always compare false to allow for indefinite movements:
-		return 1./k * log(1./c * ((1. + c)*exp(k/Vmax*time) - 1.));
+		//the problem with the below equation is that it can return infinity if k/Vmax*time is sufficiently large.
+		//return 1./k * log(1./c * ((1. + c)*exp(k/Vmax*time) - 1.));
+		//aka: 1./k*( log(1./c) + log((1. + c)*exp(k/Vmax*time) - 1.))
+		//simplify: 1./k*log(e^x-1) ~=~ 1./k*x at x = Log[1 - E^(-k*.001)], at which point it is only .001 off (however, 1ms is significant! Would rather use a smaller value.
+		auto logparam = (1. + c)*exp(k/Vmax*time);
+		if (std::isfinite(logparam)) {
+			return 1./k*( log(1./c) + log(logparam));
+		} else { //use the approximation:
+			return 1./k*(log(1./c) + log(1. + c) + k/Vmax*time);
+		}
 	}
 }
 
