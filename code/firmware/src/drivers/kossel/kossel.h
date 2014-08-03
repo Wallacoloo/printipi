7e5d70c6bad5f41a2e1b3a351367d467e5b79112
Colin Wallace <wallacoloo@gmail.com>
2014-08-03 23:04:18 +0000

    Calibration

--- code/firmware/src/drivers/kossel/kossel.h
+++ code/firmware/src/drivers/kossel/kossel.h
@@ -43,9 +43,9 @@
 
 
 #define MAX_ACCEL 600
-#define MAX_MOVE_RATE 120
+#define MAX_MOVE_RATE 160
 #define HOME_RATE 10
-#define MAX_EXT_RATE 20
+#define MAX_EXT_RATE 18
 
 #define THERM_RA 665
 #define THERM_CAP_PICO 100000
