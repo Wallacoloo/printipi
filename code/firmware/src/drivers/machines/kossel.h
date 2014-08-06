8598867043d8949941c21bd9f16bbd93b383576c
Colin Wallace <wallacoloo@gmail.com>
2014-08-06 04:50:45 +0000

    Lowered maximums

--- code/firmware/src/drivers/machines/kossel.h
+++ code/firmware/src/drivers/machines/kossel.h
@@ -43,7 +43,9 @@
 #define STEPS_M_EXT 10000*4
 
 #define MAX_ACCEL1000 600000
-#define MAX_MOVE_RATE 160
+//Can reach 160mm/sec at full-stepping (haven't tested the boundaries)
+//75mm/sec uses 75% cpu at quarter-stepping (unoptimized)
+#define MAX_MOVE_RATE 60
 #define HOME_RATE 10
 #define MAX_EXT_RATE 20
 
