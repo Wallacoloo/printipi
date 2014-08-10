037bc560769b004eb1bec1ef47c50d4b999ff3a3
Colin Wallace <wallacoloo@gmail.com>
2014-08-10 20:26:57 +0000

    Use ChronoClockPosix instead of ChronoClockRpi (Experimental)

--- code/firmware/src/common/typesettings.h
+++ code/firmware/src/common/typesettings.h
@@ -18,8 +18,10 @@
 	#ifdef COMPILING_MAIN
 		#warning "using drv::rpi::ChronoClockRpi for EventClockT. If your compilation target is not the Raspberry Pi, the program will crash on startup"
 	#endif
-	#include "drivers/rpi/chronoclockrpi.h"
-	typedef drv::rpi::ChronoClockRpi EventClockT;
+	//#include "drivers/rpi/chronoclockrpi.h"
+	//typedef drv::rpi::ChronoClockRpi EventClockT;
+	#include "boilerplate/chronoclockposix.h"
+	typedef ChronoClockPosix EventClockT;
 	#include "boilerplate/thisthreadsleepposix.h"
 	typedef ThisThreadSleepPosix SleepT;
 #else
