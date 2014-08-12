ae8c7263d1273a7208bd638075b88d30845fd154
Colin Wallace <wallacoloo@gmail.com>
2014-08-12 03:43:21 +0000

    Leveling

--- code/firmware/src/drivers/machines/kosselpi.h
+++ code/firmware/src/drivers/machines/kosselpi.h
@@ -125,9 +125,12 @@ class KosselPi : public Driver {
 		/*typedef matr::Matrix3Static<999993750, 892, -3535330, 
 892, 999999872, 505047, 
 3535330, -505047, 999993623, 1000000000> _BedLevelT; //[-0.0035, 0.0005, 0.99]*/
-		typedef matr::Matrix3Static<999989669, 1147, -4545407, 
+		/*typedef matr::Matrix3Static<999989669, 1147, -4545407, 
 1147, 999999872, 505045, 
-4545407, -505045, 999989542, 1000000000> _BedLevelT; //[-0.0045, 0.005, 0.99]
+4545407, -505045, 999989542, 1000000000> _BedLevelT; //[-0.0045, 0.0005, 0.99]*/
+		typedef matr::Matrix3Static<999975003, 1785, -7070529, 
+1785, 999999872, 505037, 
+7070529, -505037, 999974875, 1000000000> _BedLevelT; //[-0.007, 0.0005, 0.99]
     public:
         //typedef ExponentialAcceleration<MAX_ACCEL1000> AccelerationProfileT;
         typedef ConstantAcceleration<MAX_ACCEL1000> AccelerationProfileT;
