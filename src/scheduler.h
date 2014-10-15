586041eea25b7fd71515798c990b06b9ecea1aa0
Colin Wallace <wallacoloo@gmail.com>
2014-10-15 22:06:26 +0000

    Commented out some of the old, unused SchedAdjuster stuff

--- src/scheduler.h
+++ src/scheduler.h
@@ -53,25 +53,25 @@
 #include "schedulerbase.h"
 
 struct NullSchedAdjuster {
-    void reset() {}
+    //void reset() {}
     EventClockT::time_point adjust(EventClockT::time_point tp) const {
         return tp;
     }
-    void update(EventClockT::time_point) {}
+    //void update(EventClockT::time_point) {}
 };
 
-struct SchedAdjusterAccel {
-    /* The logic is a bit odd here, but the idea is to compensate for missed events.
-      If the scheduler isn't serviced on time, we don't want 10 backed-up events all happening at the same time. Instead, we offset them and pick them up then. We can never execute events with intervals smaller than they would register - this would indicate real movement of, say, 70mm/sec when the user only asked for 60mm/sec. Thus the scheduler can never be made "on track" again, unless there is a gap in scheduled events.
-      If, because of the stall, actual velocity was decreased to 10mm/sec, we cannot jump instantly back to 60mm/sec (this would certainly cause MORE missed steps)! Instead, we accelerate back up to it.
-      The tricky bit is - how do we estimate what the actual velocity is? We don't want to overcompensate. Unfortunately for now, some of the logic might :P */
+/*struct SchedAdjusterAccel {
+    // The logic is a bit odd here, but the idea is to compensate for missed events.
+    //  If the scheduler isn't serviced on time, we don't want 10 backed-up events all happening at the same time. Instead, we offset them and pick them up then. We can never execute events with intervals smaller than they would register - this would indicate real movement of, say, 70mm/sec when the user only asked for 60mm/sec. Thus the scheduler can never be made "on track" again, unless there is a gap in scheduled events.
+    //  If, because of the stall, actual velocity was decreased to 10mm/sec, we cannot jump instantly back to 60mm/sec (this would certainly cause MORE missed steps)! Instead, we accelerate back up to it.
+    //  The tricky bit is - how do we estimate what the actual velocity is? We don't want to overcompensate. Unfortunately for now, some of the logic might :P 
     //static constexpr float a = -5.0;
     static constexpr float a = -12.0;
     IntervalTimer lastRealTime;
     EventClockT::time_point lastSchedTime;
     float lastSlope;
     SchedAdjusterAccel() : lastSchedTime(), lastSlope(1) {}
-    /* Reset() should destroy any offset, so that adjust(time.now()) gives time.now() */
+    // reset() should destroy any offset, so that adjust(time.now()) gives time.now()
     void reset() {
         lastRealTime.reset();
         lastSchedTime = EventClockT::time_point();
@@ -106,7 +106,7 @@ struct SchedAdjusterAccel {
             lastSchedTime = tp;
         }
     }
-};
+};*/
 
 template <typename Interface=NullSchedulerInterface> class Scheduler : public SchedulerBase {
     typedef NullSchedAdjuster SchedAdjuster;
