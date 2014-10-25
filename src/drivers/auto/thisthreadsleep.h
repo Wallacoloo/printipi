69e611dab12d0b9797337d9d84e1b1bd95dda9fc
Colin Wallace <wallacoloo@gmail.com>
2014-10-25 06:34:29 +0000

    Allow a platform to override ThisThreadSleep as well

--- src/drivers/auto/thisthreadsleep.h
+++ src/drivers/auto/thisthreadsleep.h
@@ -3,15 +3,22 @@
 
 #include "common/typesettings/compileflags.h"
 
-#ifdef PLATFORM_DRIVER_CHRONOCLOCK
-    //custom platform clock type. Must make ALL sleeps relative (unless platform also provides ThisThreadSleep
-    #include "boilerplate/thisthreadsleepadapter.h"
-    #include "drivers/generic/thisthreadsleep.h"
-    typedef ThisThreadSleepAdapter<EventClockT, drv::generic::ThisThreadSleep> SleepT;
+#ifdef PLATFORM_DRIVER_THISTHREADSLEEP
+    #include PLATFORM_DRIVER_THISTHREADSLEEP
+    typedef drv::TARGET_PLATFORM_LOWER::ThisThreadSleep ThisThreadSleep;
 #else
-    //generic ChronoClock, so can use generic ThisThreadSleep
-    #include "drivers/generic/thisthreadsleep.h"
-    typedef drv::generic::ThisThreadSleep SleepT;
+
+    #ifdef PLATFORM_DRIVER_CHRONOCLOCK
+        //custom platform clock type. Must make ALL sleeps relative (unless platform also provides ThisThreadSleep
+        #include "boilerplate/thisthreadsleepadapter.h"
+        #include "drivers/generic/thisthreadsleep.h"
+        typedef ThisThreadSleepAdapter<EventClockT, drv::generic::ThisThreadSleep> SleepT;
+    #else
+        //generic ChronoClock, so can use generic ThisThreadSleep
+        #include "drivers/generic/thisthreadsleep.h"
+        typedef drv::generic::ThisThreadSleep SleepT;
+    #endif
+    
 #endif
 
 #endif
