71c67f01bfb94cdcfaee4c5e94f5c3cd96f5cd3d
Colin Wallace <wallacoloo@gmail.com>
2014-10-31 03:45:44 +0000

    Removed conditional compilation ifdefs - no longer necessary with build system updates

--- src/drivers/rpi/chronoclock.cpp
+++ src/drivers/rpi/chronoclock.cpp
@@ -1,7 +1,3 @@
-//Only want to initialize mitpi if we are targeting the raspberry pi:
-#include "compileflags.h"
-#ifdef DTARGET_PLATFORM_RPI
-
 #include "chronoclock.h"
 
 namespace drv {
@@ -12,4 +8,3 @@ mitpi::InitMitpiType ChronoClock::_i;
 }
 }
 
-#endif
