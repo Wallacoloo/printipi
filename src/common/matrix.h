433a1413ec79819c7dc99f46fe113a1bafbce98b
Colin Wallace <wallacoloo@gmail.com>
2014-11-25 10:44:46 +0000

    Added generic (non-static) 3x3 matrix implementation. Need to modify CoordMap usage in order to use this for bed leveling

--- src/common/matrix.h
+++ src/common/matrix.h
@@ -46,5 +46,26 @@ template <int A00_, int A01_, int A02_,
 
 typedef Matrix3Static<1, 0, 0,   0, 1, 0,   0, 0, 1> Identity3Static;
 
+class Matrix3x3 {
+    float a00, a01, a02;
+    float a10, a11, a12;
+    float a20, a21, a22;
+    public:
+        Matrix3x3() : a00(0), a01(0), a02(0), a10(0), a11(0), a12(0), a20(0), a21(0), a22(0) {}
+        Matrix3x3(float a00, float a01, float a02,
+                float a10, float a11, float a12,
+                float a20, float a21, float a22) :
+            a00(a00), a01(a01), a02(a02),
+            a10(a10), a11(a11), a12(a12),
+            a20(a20), a21(a21), a22(a22) {}
+        std::tuple<float, float, float> transform(const std::tuple<float, float, float> &xyz) const {
+            return std::tuple<float, float, float>(
+                  a00*std::get<0>(xyz) + a01*std::get<1>(xyz) + a02*std::get<2>(xyz),
+                  a10*std::get<0>(xyz) + a11*std::get<1>(xyz) + a12*std::get<2>(xyz),
+                  a20*std::get<0>(xyz) + a21*std::get<1>(xyz) + a22*std::get<2>(xyz));
+        }
+
+};
+
 }
 #endif
