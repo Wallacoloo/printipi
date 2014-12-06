83583af3ba0a3b5a57a1bc17ff49928e7b37bb1b
Colin Wallace <wallacoloo@gmail.com>
2014-12-06 22:17:50 +0000

    Added projection and norm functions

--- src/common/vector3.h
+++ src/common/vector3.h
@@ -38,8 +38,24 @@ template <typename F> class Vector3 {
 		F x() const { return _x; }
 		F y() const { return _y; }
 		F z() const { return _z; }
-		F magSq() const { return this->dot(*this); }
-		F mag() const { return sqrt(magSq()); }
+		F magSq() const { 
+			//the square of the magnitude (length) of the vector.
+			//equivalent to this->mag() * this->mag(), but less verbose and explicitly avoids the sqrt operation.
+			return this->dot(*this); 
+		}
+		F mag() const { 
+			//magnitude (length) of the vector
+			return sqrt(magSq()); 
+		}
+
+		//unary operators
+		Vector3<F> operator-() const {
+			return *this * -1;
+		}
+		Vector3<F> norm() const {
+			//Normalize the vector; return a vector of magnitude 1, but with the same direction as `this'
+			return *this / mag();
+		}
 
 		//operators:
 		Vector3<F> operator+(const Vector3<F> &v) const {
@@ -56,14 +72,28 @@ template <typename F> class Vector3 {
 		}
 
 		F dot(const Vector3<F> &v) const {
+			//The vector dot product: this . v
 			return x() * v.x() + y() * v.y() + z() * v.z();
 		}
 		Vector3<F> cross(const Vector3<F> &v) const {
+			//The vector cross product: this x v
 			//|   i   j   k   |
 			//|   ux  uy  uz  |
 			//|   vx  vy  vz  |
 			// = <uy*vz - uz*vy, uz*vx - ux*vz, ux*vy - uy*vx>
 			return Vector3<F>(y()*v.z() - z()*v.y(), z()*v.x() - x()*v.z(), x()*v.y() - y()*v.x());
+		}
+		F scalarProj(const Vector3<F> &v) const {
+			//the scalar projection of `this' onto v.
+			//That is, the scalar component of `this' in the direction of v.
+			return this->dot(v) / v.mag();
+		}
+		Vector3<F> proj(const Vector3<F> &v) const {
+			//projection of `this' onto v.
+			//That is, the vector componenent of `this' in the direction of v.
+			//This is equivalent to (this->scalarProj(v)) * v.norm(),
+			//  but we explicitly avoid the sqrt operations
+			return v * (this->dot(v) / v.magSq());
 		}
 };
 
