b3f58b9a0ccd69449252fe0b92908191e7f148ee
Colin Wallace <wallacoloo@gmail.com>
2014-12-14 05:37:22 +0000

    Update travis for multiple compiler versions

--- buildtest.sh
+++ buildtest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pushd src
-sudo apt-get install -qq g++-4.6 g++-4.7
 make CXX=g++-4.6 MACHINE=rpi/kosselrampsfd.h
 make CXX=g++-4.6 MACHINE=generic/example.h
 make CXX=g++-4.7 MACHINE=rpi/kosselrampsfd.h
