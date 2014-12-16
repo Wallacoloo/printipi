20f856833679bd9c4907c02ca58387278476c8c3
Colin Wallace <wallacoloo@gmail.com>
2014-12-16 11:34:51 +0000

    Update for new machines

--- buildtest.sh
+++ buildtest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#This script tests a few different build settings to make sure nothing's broken
+#This script is called from .travis.yml after each push to github.
 set -e #exit script if any of the commands error
 pushd src
 make clean
 make CXX=g++-4.6 MACHINE=rpi/kosselrampsfd.h
-make CXX=g++-4.6 MACHINE=generic/example.h
+make CXX=g++-4.6 MACHINE=generic/cartesian.h
 make CXX=g++-4.7 MACHINE=rpi/kosselrampsfd.h
-make CXX=g++-4.7 MACHINE=generic/example.h
+make CXX=g++-4.7 MACHINE=generic/cartesian.h
 popd
