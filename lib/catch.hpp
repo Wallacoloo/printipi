9097afd40967e84211ce5d2bb5245bc13609462b
Colin Wallace <wallacoloo@gmail.com>
2014-12-25 07:47:16 +0000

    Add catch wrapper

---
+++ lib/catch.hpp
@@ -0,0 +1,3 @@
+//Simple wrapper file to conditionally include the real CATCH for if we're doing a build with tests enabled.
+//in the future, replace CATCH macros with dummies if tests are to be disabled.
+#include "catch/catch.hpp"
