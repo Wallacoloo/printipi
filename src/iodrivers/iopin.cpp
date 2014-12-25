8cdf9997eaa9262d776053ca230e26596910cd30
Colin Wallace <wallacoloo@gmail.com>
2014-12-25 12:43:48 +0000

    Added some basic test fixtures

--- src/iodrivers/iopin.cpp
+++ src/iodrivers/iopin.cpp
@@ -2,6 +2,7 @@
 
 #include "schedulerbase.h" //for SchedulerBase::registerExitHandler
 #include "common/logging.h"
+#include "catch.hpp" //for the testsuite
 
 namespace iodrv {
 
@@ -42,4 +43,19 @@ IoPin& IoPin::operator=(IoPin &&other) {
     return *this;
 }
 
+}
+
+TEST_CASE("IoPins will correctly invert writes", "[iopin]") {
+    SECTION("Inverted reads won't invert writes") {
+        iodrv::IoPin p(iodrv::INVERT_READS, IoDefaultLow, PrimitiveIoPin::null());
+        REQUIRE(p.translateWriteToPrimitive(IoLow) == IoLow);
+        REQUIRE(p.translateWriteToPrimitive(IoHigh) == IoHigh);
+        REQUIRE(p.translateDutyCycleToPrimitive(0.2) == Approx(0.2));
+    }
+    SECTION("Inverted pins will invert writes") {
+        iodrv::IoPin p(iodrv::INVERT_WRITES, IoDefaultLow, PrimitiveIoPin::null());
+        REQUIRE(p.translateWriteToPrimitive(IoLow) == IoHigh);
+        REQUIRE(p.translateWriteToPrimitive(IoHigh) == IoLow);
+        REQUIRE(p.translateDutyCycleToPrimitive(0.2) == Approx(0.8));
+    }
 }
