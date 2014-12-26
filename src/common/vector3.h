fc0ef9a840d8729d38ee3728086626d955782a1b
Colin Wallace <wallacoloo@gmail.com>
2014-12-26 07:12:58 +0000

    inline not needed for template function definitions

--- src/common/vector3.h
+++ src/common/vector3.h
@@ -29,68 +29,68 @@ template <typename F> class Vector3 {
 	//mathematical vector utility
 	F _x, _y, _z;
 	public:
-		inline Vector3() : _x(0), _y(0), _z(0) {}
-		inline Vector3(F x, F y, F z) : _x(x), _y(y), _z(z) {}
+		Vector3() : _x(0), _y(0), _z(0) {}
+		Vector3(F x, F y, F z) : _x(x), _y(y), _z(z) {}
 		template <typename Other> Vector3(const Other &v)
 		  : _x(v.x()), _y(v.y()), _z(v.z()) {}
 
 		//accessors:
-		inline F x() const { return _x; }
-		inline F y() const { return _y; }
-		inline F z() const { return _z; }
-		inline F magSq() const { 
+		F x() const { return _x; }
+		F y() const { return _y; }
+		F z() const { return _z; }
+		F magSq() const { 
 			//the square of the magnitude (length) of the vector.
 			//equivalent to this->mag() * this->mag(), but less verbose and explicitly avoids the sqrt operation.
 			return this->dot(*this); 
 		}
-		inline F mag() const { 
+		F mag() const { 
 			//magnitude (length) of the vector
 			return sqrt(magSq()); 
 		}
 
 		//unary operators
-		inline Vector3<F> operator-() const {
+		Vector3<F> operator-() const {
 			return *this * -1;
 		}
-		inline Vector3<F> norm() const {
+		Vector3<F> norm() const {
 			//Normalize the vector; return a vector of magnitude 1, but with the same direction as `this'
 			return *this / mag();
 		}
 
 		//operators:
-		inline Vector3<F> operator+(const Vector3<F> &v) const {
+		Vector3<F> operator+(const Vector3<F> &v) const {
 			return Vector3<F>(x() + v.x(), y() + v.y(), z() + v.z());
 		}
-		inline Vector3<F>& operator+=(const Vector3<F> &v) {
+		Vector3<F>& operator+=(const Vector3<F> &v) {
 			return *this = (*this + v);
 		}
 
-		inline Vector3<F> operator-(const Vector3<F> &v) const {
+		Vector3<F> operator-(const Vector3<F> &v) const {
 			return *this + (-v);
 		}
-		inline Vector3<F>& operator-=(const Vector3<F> &v) {
+		Vector3<F>& operator-=(const Vector3<F> &v) {
 			return *this = (*this - v);
 		}
 
-		inline Vector3<F> operator*(F s) const {
+		Vector3<F> operator*(F s) const {
 			return Vector3<F>(s*x(), s*y(), s*z());
 		}
-		inline Vector3<F>& operator*=(F s) {
+		Vector3<F>& operator*=(F s) {
 			return *this = (*this * s);
 		}
 
-		inline Vector3<F> operator/(F s) const {
+		Vector3<F> operator/(F s) const {
 			return *this * ((F)1. / s);
 		}
-		inline Vector3<F>& operator/=(F s) {
+		Vector3<F>& operator/=(F s) {
 			return *this = (*this / s);
 		}
 
-		inline F dot(const Vector3<F> &v) const {
+		F dot(const Vector3<F> &v) const {
 			//The vector dot product: this . v
 			return x() * v.x() + y() * v.y() + z() * v.z();
 		}
-		inline Vector3<F> cross(const Vector3<F> &v) const {
+		Vector3<F> cross(const Vector3<F> &v) const {
 			//The vector cross product: this x v
 			//|   i   j   k   |
 			//|   ux  uy  uz  |
@@ -98,12 +98,12 @@ template <typename F> class Vector3 {
 			// = <uy*vz - uz*vy, uz*vx - ux*vz, ux*vy - uy*vx>
 			return Vector3<F>(y()*v.z() - z()*v.y(), z()*v.x() - x()*v.z(), x()*v.y() - y()*v.x());
 		}
-		inline F scalarProj(const Vector3<F> &v) const {
+		F scalarProj(const Vector3<F> &v) const {
 			//the scalar projection of `this' onto v.
 			//That is, the scalar component of `this' in the direction of v.
 			return this->dot(v) / v.mag();
 		}
-		inline Vector3<F> proj(const Vector3<F> &v) const {
+		Vector3<F> proj(const Vector3<F> &v) const {
 			//projection of `this' onto v.
 			//That is, the vector componenent of `this' in the direction of v.
 			//This is equivalent to (this->scalarProj(v)) * v.norm(),
