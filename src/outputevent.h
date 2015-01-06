f277ef5d655d28c170a9a831bf52b6790232e6c4
Colin Wallace <wallacoloo@gmail.com>
2015-01-06 19:51:56 +0000

    Improve documentation

--- src/outputevent.h
+++ src/outputevent.h
@@ -38,24 +38,37 @@ class OutputEvent {
     PrimitiveIoPin _pin;
     IoLevel _state;
     public:
+        //default constructor. Creates an OutputEvent where isNull() will return true
         inline OutputEvent()
         : _time(std::chrono::seconds(0)), _pin(PrimitiveIoPin::null()), _state(IoLow) {
         }
+        //Construct from a time point, a pin and a state.
+        //@time the time at which the pin state should be altered.
+        //@state the logical state the pin should be put in *before* any inversions have been processed.
+        //  so if @state is IoHigh, pin.areWritesInverted() == true, then the physical hardware pin will output LOW (0V / Ground)
         inline OutputEvent(EventClockT::time_point time, const iodrv::IoPin &pin, bool state) 
         : _time(time), _pin(pin.primitiveIoPin()), _state(pin.translateWriteToPrimitive(state)) {
         }
+        //@return the time at which the pin state should be altered.
         inline EventClockT::time_point time() const {
             return _time;
         }
+        //@return the pin that should be affected.
         inline const PrimitiveIoPin& primitiveIoPin() const {
             return _pin;
         }
+        //@return the pin that should be affected.
         inline PrimitiveIoPin& primitiveIoPin() {
             return _pin;
         }
+        //@return the state that the pin should be set to.
+        //  Note: since this is a PrimitiveIoPin, any inversions have already been processed.
+        //    so if state() returns IoHigh, then the pin should output 3.3v (or 5v, etc).
+        //    if IoLow, then pin should output 0v (ground)
         inline bool state() const {
             return _state;
         }
+        //@return true if the OutputEvent was default-constructed.
         inline bool isNull() const {
             return _time == EventClockT::time_point(std::chrono::seconds(0));
         }
