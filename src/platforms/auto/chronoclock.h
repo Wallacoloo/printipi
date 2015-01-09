7a4e6cc47c7c9b8f454472fe552064148b69af64
Colin Wallace <wallacoloo@gmail.com>
2015-01-09 04:35:38 +0000

    Fix outdated path in warning message

--- src/platforms/auto/chronoclock.h
+++ src/platforms/auto/chronoclock.h
@@ -31,7 +31,7 @@
     typedef plat::TARGET_PLATFORM_LOWER::ChronoClock EventClockT;
 #else
     #ifdef COMPILING_MAIN
-        #warning "using ChronoClockPosix for EventClockT. While this does work, you will get better performance if you use a clock specific to your machine (implement src/drivers/<PLATFORM>/chronoclock.h)"
+        #warning "using ChronoClockPosix for EventClockT. While this does work, you will get better performance if you use a clock specific to your machine (implement src/platforms/<PLATFORM>/chronoclock.h)"
     #endif
     #include "platforms/generic/chronoclock.h"
     typedef plat::generic::ChronoClock EventClockT;
