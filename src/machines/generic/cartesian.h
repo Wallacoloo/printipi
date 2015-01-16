cc1856dc5e2ecc02433d6a86d3933a0fdc0a9fdf
Colin Wallace <wallacoloo@gmail.com>
2015-01-16 05:46:20 +0000

    Use RCThermistor2Pin instead of RCThermistor

--- src/machines/generic/cartesian.h
+++ src/machines/generic/cartesian.h
@@ -43,7 +43,7 @@
 #include "iodrivers/a4988.h"
 #include "iodrivers/fan.h"
 #include "iodrivers/tempcontrol.h"
-#include "iodrivers/rcthermistor.h"
+#include "iodrivers/rcthermistor2pin.h"
 #include "pid.h"
 #include "common/filters/lowpassfilter.h"
 #include "iodrivers/servo.h"
@@ -72,11 +72,19 @@
 #define PIN_ENDSTOP_Y             -1
 #define PIN_ENDSTOP_Z             -1
 #define PIN_ENDSTOP_INVERSIONS    NO_INVERSIONS           //if not inverted, endstops are HIGH when active
-#define PIN_THERMISTOR            -1  
+
+//Refer to the RcThermistor2Pin documentation.
+//One pin is used to discharge the capacitor through the thermistor (variable resistance)
+//  This pin should be connected through the thermistor to P301-2
+#define PIN_THERMISTOR            -1
+//One pin (fixed resistance) is used for charging the capacitor.
+//  This pin should be connected through a ~1kohm resistor to THERM0 (AD0)
+#define PIN_THERMISTOR_CHARGE     -1
 #define PIN_FAN                   -1    
 #define PIN_FAN_INVERSIONS        INVERT_WRITES
 #define PIN_FAN_DEFAULT_STATE     IO_DEFAULT_LOW
 #define FAN_MIN_PWM_PERIOD        0.01                    
+
 #define PIN_HOTEND                -1    
 #define PIN_HOTEND_INVERSIONS     NO_INVERSIONS
 #define HOTEND_MIN_PWM_PERIOD     0.01                    //MOSFETS have a limited switching frequency
@@ -106,14 +114,17 @@
 #define HOTEND_PID_I  0.250
 #define HOTEND_PID_D  1.000
 
-//Resistor-Capacitor thermistor read settings (see iodrivers/rcthermistor.h):
-#define THERM_RA_OHMS     665
-#define THERM_CAP_FARADS  2.2e-6
-#define VCC_V             3.300
-#define THERM_IN_THRESH_V 1.600
-#define THERM_T0_C        25
-#define THERM_R0_OHMS     100000
-#define THERM_BETA        3950
+//Resistor-Capacitor thermistor read settings (see iodrivers/rcthermistor2pin.h):
+#define THERM_C_FARADS            10.10e-6
+#define THERM_V_TOGGLE_V          1.27
+#define THERM_RCHARGE_OHMS        1000
+#define THERM_RSERIES_OHMS          22
+#define THERM_RUP_OHMS            4700
+#define THERM_T0_C                25.0
+#define THERM_R0_OHMS             100000
+#define THERM_BETA                3950
+
+#define VCC_V                     3.3  
 
 namespace machines {
 namespace generic {
@@ -149,19 +160,28 @@ class cartesian : public Machine {
             0, 1, 0,
             0, 0, 1));
         }
-        inline std::tuple<Fan, Servo, TempControl<RCThermistor, PID, LowPassFilter> > 
+        inline std::tuple<Fan, Servo, TempControl<RCThermistor2Pin, PID, LowPassFilter> > 
           getIoDrivers() const {
             return std::make_tuple(
                 Fan(IoPin(PIN_FAN_INVERSIONS, PIN_FAN), PIN_FAN_DEFAULT_STATE, FAN_MIN_PWM_PERIOD),
                 Servo(IoPin::null(), std::chrono::milliseconds(100), 
                     std::make_pair(std::chrono::milliseconds(1), std::chrono::milliseconds(2)),
                     std::make_pair(0.0f, 360.0f)),
-                TempControl<RCThermistor, PID, LowPassFilter>(
+                TempControl<RCThermistor2Pin, PID, LowPassFilter>(
                     iodrv::HotendType,
                     IoPin(PIN_HOTEND_INVERSIONS, PIN_HOTEND), 
-                    RCThermistor(
+                    RCThermistor2Pin(
                         IoPin(NO_INVERSIONS, PIN_THERMISTOR),
-                        THERM_RA_OHMS, THERM_CAP_FARADS, VCC_V, THERM_IN_THRESH_V, THERM_T0_C, THERM_R0_OHMS, THERM_BETA), 
+                        IoPin(NO_INVERSIONS, PIN_THERMISTOR_CHARGE),
+                        THERM_RCHARGE_OHMS, 
+                        THERM_RSERIES_OHMS,
+                        THERM_RUP_OHMS,
+                        THERM_C_FARADS, 
+                        VCC_V, 
+                        THERM_V_TOGGLE_V, 
+                        THERM_T0_C, 
+                        THERM_R0_OHMS, 
+                        THERM_BETA), 
                     PID(HOTEND_PID_P, HOTEND_PID_I, HOTEND_PID_D), 
                     LowPassFilter(3.000)
                 ));
